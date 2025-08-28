bebcb835acd20bd7f7c63be9a610db5e7e19f342
Ayanda Dlamini <Janicedoe745@gmail.com>
2025-08-28 13:02:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 function displayPoem(response) {
-    new Typewriter("#poem-container", {
-  strings: response.data.answer,
-  autoStart: true,
-  delay:1,
-});
+  
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 30,
+  });
 }
-
-
 
 function generatePoem(event) {
-    event.preventDefault();
-  
+  event.preventDefault();
 
+  let userInput = document.querySelector("#userInput");
+  let ApiKey = "ad28f3a0557d8t5f574o89b184356e5a";
+  let context =
+    "You are a helpful assistant that generates beautiful poems based on a given topic.";
+  let prompt = `Write a poem about ${userInput.value} in 4 lines.`;
 
-let userInput = document.querySelector("#userInput");
-let ApiKey="ad28f3a0557d8t5f574o89b184356e5a";
-let context="You are a helpful assistant that generates beautiful poems based on a given topic.";
-let prompt=`Write a poem about ${userInput.value} in 4 lines.`;
-let ApiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
+  let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
 
-console.log("Generating poem about: " + userInput.value);
-console.log(`prompt: ${prompt}`);
-console.log(`context: ${context}`);
-axios.get(ApiUrl).then(displayPoem);
-
+  console.log("Generating poem about: " + userInput.value);
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+  axios.get(ApiUrl).then(displayPoem);
 }
-let poemFormElement = document.querySelector('.poem-generator');
+let poemElement= document.querySelector(".poem");
+  poemElement.classList.remove(`hidden`);
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${userInput.value}</div>`;
+
+let poemFormElement = document.querySelector(".poem-generator");
 poemFormElement.addEventListener("submit", generatePoem);
