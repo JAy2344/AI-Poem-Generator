8faf2189699a4ade604357c16488b7fde9ab94c4
Ayanda Dlamini <Janicedoe745@gmail.com>
2025-08-28 09:19:13 +0200

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
+function displayPoem(response) {
+    new Typewriter("#poem-container", {
+  strings: response.data.answer,
+  autoStart: true,
+  delay:1,
+});
+}
+
+
+
 function generatePoem(event) {
     event.preventDefault();
   
 
 
-new Typewriter("#poem-container", {
-  strings: "Pink, a color of tenderness, sweetness, and love, is found in delicate things like a rose's blush, a baby's soft skin, and the pastel glow of dawn",
-  autoStart: true,
-  delay:1,
-});
+let userInput = document.querySelector("#userInput");
+let ApiKey="ad28f3a0557d8t5f574o89b184356e5a";
+let context="You are a helpful assistant that generates beautiful poems based on a given topic.";
+let prompt=`Write a poem about ${userInput.value} in 4 lines.`;
+let ApiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
+
+console.log("Generating poem about: " + userInput.value);
+console.log(`prompt: ${prompt}`);
+console.log(`context: ${context}`);
+axios.get(ApiUrl).then(displayPoem);
+
 }
 let poemFormElement = document.querySelector('.poem-generator');
 poemFormElement.addEventListener("submit", generatePoem);
