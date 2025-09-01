a084185760f561dbf4c8301a1cdb972e47e04ab2
Ayanda Dlamini <Janicedoe745@gmail.com>
2025-09-01 07:45:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 function displayPoem(response) {
+
+  let poemElement= document.querySelector(".poem");
+  poemElement.classList.remove(`hidden`);
+  poemElement.innerHTML = " ";
+
   
   new Typewriter("#poem", {
     strings: response.data.answer,
@@ -12,7 +17,7 @@ function generatePoem(event) {
 
   let userInput = document.querySelector("#userInput");
   let ApiKey = "ad28f3a0557d8t5f574o89b184356e5a";
-  let context ="You are a helpful assistant that generates beautiful poems based on a given topic. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. include a title to the poem. do not include ```html ";
+  let context ="You are a helpful assistant that generates beautiful poems based on a given topic. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. include a title to the poem. do not include ```html . add a signature at the end of the poem with <br /> - Your AI Poet";
   let prompt = `Write a poem about ${userInput.value} in 4 lines.`;
 
   let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
@@ -20,11 +25,15 @@ function generatePoem(event) {
   //console.log("Generating poem about: " + userInput.value);
   //console.log(`prompt: ${prompt}`);
   //console.log(`context: ${context}`);
-  axios.get(ApiUrl).then(displayPoem);
-}
-let poemElement= document.querySelector(".poem");
+
+  let poemElement= document.querySelector(".poem");
   poemElement.classList.remove(`hidden`);
   poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${userInput.value}</div>`;
+
+  axios.get(ApiUrl).then(displayPoem);
+}
+
+
 
 let poemFormElement = document.querySelector(".poem-generator");
 poemFormElement.addEventListener("submit", generatePoem);
